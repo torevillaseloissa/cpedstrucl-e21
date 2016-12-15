38769a55eba78c57699c139123d81eb4abf2e6c1
torevillaseloissa <eloi.torev@yahoo.com>
2016-12-15 09:45:25 +0800

    Update linked_list.cpp

--- linked_list.cpp
+++ linked_list.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
+#include <conio.h>
 using namespace std;
 
+int choice, x, z;
 class LinkedList{
     struct Node {
         int x;
@@ -13,11 +15,13 @@ public:
     }
 
     void addValue(int val){
-        Node *n = new Node();   
+    	cout << "\tEnter number: ";
+    	cin >> val;
+    	Node *n = new Node();   
         n->x = val;             
         n->next = head;         
                                
-        head = n;              
+        head = n;             
     }
 
     int popValue(){
@@ -28,6 +32,58 @@ public:
         delete n;
         return ret;
     }
+   	int traverse(){    
+		Node *n = head;
+        int ret = n->x;
+
+        head = head->next;
+        for(int i=0; i<z; i++)
+        return ret; 		
+	}
+
+    
+    void menu(){
+		system("cls");
+    	cout << "\t\t\tMenu" << endl;
+    	cout << "\t[1] Add" << endl;
+    	cout << "\t[2] Pop" << endl;
+    	cout << "\t[3] Traverse" << endl;
+    	cout << "\t[4] Exit" << endl;
+    	cout << "\tEnter your choice: ";
+    	cin >> choice;
+	}
+	
+	void cases( int c){
+		switch(c)
+		{
+			case 1:
+				 addValue(x);
+				 z++;
+				 break;
+			case 2:
+				if(z==0)
+				{
+					cout << "\tEMPTY!";
+				}
+				else
+				{
+				cout << "\tYou Pop the Value: " << popValue() << endl;
+				z--;
+				}
+				break;
+			case 3:
+				 cout << traverse();
+				 break;
+			case 4:
+				exit(1);
+				break;
+			default:
+				cout << "\tInvalid!" << endl;
+				break;
+		}
+		system("pause");
+		getch();		
+	}
 
 private:
     Node *head; 
@@ -36,11 +92,18 @@ private:
 int main() {
     LinkedList list;
 
-    list.addValue(5);
+  /*	list.addValue(5);
     list.addValue(10);
     list.addValue(20);
+    list.menu();
+*/
+while(1)
+{
 
-    cout << list.popValue() << endl;
+	list.menu();
+	list.cases(choice);
+}
+   // cout << list.popValue() << endl;
     cout << list.popValue() << endl;
     cout << list.popValue() << endl;
     return 0;
